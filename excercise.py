27061b98550c8e08b057400168154b4be81a394d
Vikil Naik <vikilnaik37@gmail.com>
2019-02-28 12:41:44 -0500

    Finished excercise

---
+++ excercise.py
@@ -0,0 +1,8 @@
+def word_count(word):
+    return len(word.split())
+
+print(word_count("Hello Word")) # returns 2
+
+print(word_count("This is a sentence")) # returns 3
+
+print(word_count("")) # returns 0
